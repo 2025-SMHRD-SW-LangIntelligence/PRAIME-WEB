5f0bfd36e0c78571ad52cb0a1fb978796b685a06
ardor924 <106001182+ardor924@users.noreply.github.com>
2025-06-11 17:09:45 +0900

    [0611] Build : 스프링부트 프로젝트 빌드 및 작동테스트

---
+++ PRAIME/src/main/java/com/example/praime/controller/PageController.java
@@ -0,0 +1,11 @@
+package com.example.praime.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import ch.qos.logback.core.model.Model;
+
+
+public class PageController {
+	
+
+	
+}
