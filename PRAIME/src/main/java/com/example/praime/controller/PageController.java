ea7ea4d01ef2ed0a2c0d55997d455a8cdb74c2d0
ardor924 <106001182+ardor924@users.noreply.github.com>
2025-06-11 19:18:47 +0900

    [0611] Test : dev브랜치 등록 테스트

--- PRAIME/src/main/java/com/example/praime/controller/PageController.java
+++ PRAIME/src/main/java/com/example/praime/controller/PageController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PageController {
 	
-	// 메인페이지
+	// 메인페이지 이동
 	@GetMapping(value = "/")
 	public String home() {
 		return "main";
