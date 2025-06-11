3ff6662e82991a7a9edc1f1e38c6b12dc6a48037
ardor924 <106001182+ardor924@users.noreply.github.com>
2025-06-11 20:06:33 +0900

    Feat : 페이지컨트롤러 - 회원가입페이지 이동 추가

--- PRAIME/src/main/java/com/smhrd/praime/controller/PageController.java
+++ PRAIME/src/main/java/com/smhrd/praime/controller/PageController.java
@@ -4,12 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class MainController {
+public class PageController {
 	
 	// 메인페이지 이동
 	@GetMapping(value = "/")
 	public String mainPage() {
 		return "main";
+	}
+	
+	// 회원가입 페이지 이동
+	@GetMapping(value = "/")
+	public String joinPage() {
+		return "join";
 	}	
 	
 }
