96524df36b0867ecacdcaeff8dc41edd9b5f5bc7
이은석 <hspup3@gmail.com>
2025-06-24 21:50:19 +0900

    충돌나라

--- PRAIME/src/main/java/com/smhrd/praime/repository/DiagnosisRepository.java
+++ PRAIME/src/main/java/com/smhrd/praime/repository/DiagnosisRepository.java
@@ -48,6 +48,7 @@ public interface DiagnosisRepository extends JpaRepository<DiagnosisEntity, Long
     
     /**
      * .
+     * ....
      * 특정 기간 내 진단 결과 조회
      */
     @Query("SELECT d FROM DiagnosisEntity d WHERE d.createdAt BETWEEN :startDate AND :endDate ORDER BY d.createdAt DESC")
