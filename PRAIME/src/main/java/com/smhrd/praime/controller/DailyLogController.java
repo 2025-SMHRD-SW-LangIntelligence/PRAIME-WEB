fe421f80c4dbf4809a7181fdc11390bbd88b1c29
ardor924 <106001182+ardor924@users.noreply.github.com>
2025-06-25 20:50:23 +0900

    Feat : 리팩토링 적용시 임포트 안된것 수정

--- PRAIME/src/main/java/com/smhrd/praime/controller/DailyLogController.java
+++ PRAIME/src/main/java/com/smhrd/praime/controller/DailyLogController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
